5e3c8d1817303c050a78629224961a913c5497c9
Ezeh Godwin <ezehgodwin3942@gmail.com>
2024-02-20 11:50:25 -0800

    updated the design

--- script/TaskList.js
+++ script/TaskList.js
@@ -4,6 +4,10 @@ const popUp = document.querySelector(".popUp-text");
 const editModal = document.querySelector(".modal");
 const editBtn = document.querySelector(".editBtn");
 const cancleBtn = document.querySelector(".cancleBtn");
+let arrayTask = [];
+
+
+
 
 
 
@@ -14,6 +18,7 @@ const TaskList = () => {
     let tasks = task.value.trim()
     let date = Date.now();
 
+
     if (titles === "" || tasks === "") {
         popUp.textContent = "input feild empty!!";
         popUp.style.color = "red";
@@ -23,15 +28,138 @@ const TaskList = () => {
             popUp.style.marginLeft = "100%"
         }, 1500);
     } else {
+            
+            // const arrayTask = [];
+            const taskObj = Object.create({});
+            taskObj.id = date;
+            taskObj.taskTitle = titles;
+            taskObj.taskTask = tasks;
 
-        // const newTask = Object.create({});
-        // newTask.id = date;
-        // newTask.newTitle = titles;
-        // newTask.newTasks = tasks
+             arrayTask.push(taskObj)
 
-        // localStorage.setItem("myLocalStore",JSON.stringify(newTask));
-        // const mylocalData = JSON.parse(localStorage.getItem("myLocalStore"));
-        // console.log(mylocalData);
+        // tag creation
+    //                 const article = document.createElement("article");
+    //                 const h2 = document.createElement("h2"); 
+    //                 const blockquote = document.createElement("blockquote");
+    //                 const span = document.createElement("span");
+    //                 const p = document.createElement("p");
+    //                 const iFirst = document.createElement("i");
+    //                 const iSecond = document.createElement("i");
+    //                 const iThird = document.createElement("i");
+    //         // adding classs names
+    //             iFirst.className = "fas fa-check-double";
+    //             iSecond.className = "fas fa-edit";
+    //             iThird.className = "fas fa-trash-alt";
+    //             span.className = "task-header";
+    //             p.className = "icon";
+
+
+    //     const newTask = Object.create({});
+    //     newTask.id = date;
+    //     newTask.newTitle = titles;
+    //     newTask.newTasks = tasks
+
+    //    arrayTask.push(newTask);
+
+    //    popUp.textContent = "task added successfully";
+    //        popUp.style.color = "green";
+    //        popUp.style.backgroundColor = "#8bfecc"
+    //        popUp.style.marginLeft = "0%"
+    //        setTimeout(() => {
+    //            popUp.style.marginLeft = "100%"
+    //        }, 1500);
+
+    //     localStorage.setItem("myLocalStore",JSON.stringify(arrayTask));
+    //     const mylocalData = JSON.parse(localStorage.getItem("myLocalStore"));
+    //        console.log(mylocalData);
+    //     mylocalData.forEach(eachTask => {
+    //         const {id,newTitle,newTask} = eachTask;
+
+    //         // added the textContext
+    //             h2.textContent = `${newTitle}`; 
+    //             blockquote.textContent = `${newTask}`;
+                
+    //             const taskComplete = () => {
+    //                     const taskId = mylocalData.find( (eachTasks) => eachTasks.id === eachTasks.id)
+    //                      console.log(taskId)
+
+    //                     // article.style.backgroundColor = "green";
+    //                     // setTimeout(() => {
+    //                     //     article.style.backgroundColor = "#fff";
+    //                     //     iFirst.style.color = "green"
+    //                     // }, 1500);
+    //                     // popUp.textContent = "task completed successfully";
+    //                     // popUp.style.color = "green";
+    //                     // popUp.style.backgroundColor = "#8bfecc"
+    //                     // popUp.style.marginLeft = "0%"
+    //                     // setTimeout(() => {
+    //                     //     popUp.style.marginLeft = "100%"
+    //                     // }, 1500);
+    //             };
+    //             iFirst.addEventListener("click",taskComplete);
+    //     });
+
+    //     // mylocalData.map((eachTask) => {
+
+    //     //           // added the textContext
+    //     //             h2.textContent = `${eachTask.newTitle}`; 
+    //     //             blockquote.textContent = `${eachTask.newTask}`;
+    //     //             const taskComplete = () => {
+    //     //                 const taskId = mylocalData.find( (eachTasks) => eachTasks.id === eachTasks.id)
+    //     //                  console.log(taskId)
+            
+    //     //                      // article.style.backgroundColor = "green";
+    //     //                      // setTimeout(() => {
+    //     //                      //     article.style.backgroundColor = "#fff";
+    //     //                      //     iFirst.style.color = "green"
+    //     //                      // }, 1500);
+    //     //                      // popUp.textContent = "task completed successfully";
+    //     //                      // popUp.style.color = "green";
+    //     //                      // popUp.style.backgroundColor = "#8bfecc"
+    //     //                      // popUp.style.marginLeft = "0%"
+    //     //                      // setTimeout(() => {
+    //     //                      //     popUp.style.marginLeft = "100%"
+    //     //                      // }, 1500);
+    //     //              };
+    //     //              iFirst.addEventListener("click",taskComplete);
+    //     // })
+    //        // appended the tag to the html tag
+    //             p.appendChild(iFirst);
+    //             p.appendChild(iSecond);
+    //             p.appendChild(iThird);
+
+    //             span.appendChild(h2);
+    //             span.appendChild(p);
+                
+    //             article.appendChild(span);
+    //             article.appendChild(blockquote);
+    //             div.appendChild(article);
+
+    //     title.value = ""
+    //     task.value = ""
+
+    //      // icon functions
+    //     //  const taskComplete = () => {
+    //     //     // const taskId = mylocalData.find( (eachTasks) => eachTasks.id === id)
+    //     //     //  console.log(taskId)
+
+    //     //          // article.style.backgroundColor = "green";
+    //     //          // setTimeout(() => {
+    //     //          //     article.style.backgroundColor = "#fff";
+    //     //          //     iFirst.style.color = "green"
+    //     //          // }, 1500);
+    //     //          // popUp.textContent = "task completed successfully";
+    //     //          // popUp.style.color = "green";
+    //     //          // popUp.style.backgroundColor = "#8bfecc"
+    //     //          // popUp.style.marginLeft = "0%"
+    //     //          // setTimeout(() => {
+    //     //          //     popUp.style.marginLeft = "100%"
+    //     //          // }, 1500);
+    //     //  };
+    //     //  iFirst.addEventListener("click",taskComplete);
+     
+
+            
 
         // normal task function
         // // tag creation
@@ -49,89 +177,90 @@ const TaskList = () => {
         iThird.className = "fas fa-trash-alt";
         span.className = "task-header";
         p.className = "icon";
-        // pop up 
-        popUp.textContent = "task added successfully";
-        popUp.style.color = "green";
-        popUp.style.backgroundColor = "#8bfecc"
-        popUp.style.marginLeft = "0%"
-        setTimeout(() => {
-            popUp.style.marginLeft = "100%"
-        }, 1500);
 
-        // added the textContext
-        h2.textContent = `${titles}`; 
-        blockquote.textContent = `${tasks}`;
-        // appended the tag to the html tag
-        p.appendChild(iFirst);
-        p.appendChild(iSecond);
-        p.appendChild(iThird);
+      
 
-        span.appendChild(h2);
-        span.appendChild(p);
-        
-        article.appendChild(span);
-        article.appendChild(blockquote);
-        div.appendChild(article);
-
-        // localStorage.setItem("myLocalStore",JSON.stringify(article));
-        // const mylocalData = JSON.parse(localStorage.getItem("myLocalStore"));
-        // console.log(mylocalData);
-
-    // clearing the input
-       title.value = "";
-       task.value = "";
-    //    icon functions
-       const taskComplete = () => {
-            article.style.backgroundColor = "green";
-            setTimeout(() => {
-                article.style.backgroundColor = "#fff";
-                iFirst.style.color = "green"
-            }, 1500);
-            popUp.textContent = "task completed successfully";
+    arrayTask.forEach((singleTask) => {
+        // added the textContext;
+          article.key = `${singleTask.id}`
+          h2.textContent = `${singleTask.taskTitle}`; 
+          blockquote.textContent = `${singleTask.taskTask}`;
+           // pop up 
+            popUp.textContent = "task added successfully";
             popUp.style.color = "green";
             popUp.style.backgroundColor = "#8bfecc"
             popUp.style.marginLeft = "0%"
             setTimeout(() => {
                 popUp.style.marginLeft = "100%"
             }, 1500);
-       };
+            // appended the tag to the html tag
+            p.appendChild(iFirst);
+            p.appendChild(iSecond);
+            p.appendChild(iThird);
+
+            span.appendChild(h2);
+            span.appendChild(p);
+            
+            article.appendChild(span);
+            article.appendChild(blockquote);
+            div.appendChild(article);
+
+            // clearing the input
+            title.value = "";
+            task.value = "";
+                   
+    })
+
+    const taskComplete = () => {
+
+        // const completed = arrayTask.find(tasks => "id" in tasks)
+        // console.log(completed.taskTitle);
+        
+           article.style.backgroundColor = "green";
+           setTimeout(() => {
+               article.style.backgroundColor = "#fff";
+               iFirst.style.color = "green"
+           }, 1500);
+           popUp.textContent = "task completed successfully";
+           popUp.style.color = "green";
+           popUp.style.backgroundColor = "#8bfecc"
+           popUp.style.marginLeft = "0%"
+           setTimeout(() => {
+               popUp.style.marginLeft = "100%"
+           }, 1500);
+      };
+
+    // localStorage.setItem("myLocalStore",JSON.stringify(arrayTask));
+    // const mylocalData = JSON.parse(localStorage.getItem("myLocalStore"));
+    // console.log(mylocalData);
+
 
        const taskEdit = () => {
-        const editTitle = document.getElementById("editTitle")
-        const editTask = document.getElementById("editTask")
-        let editTitles = editTitle.value.trim()
-        let editTasks = editTask.value.trim()
+         title.value = titles;
+         task.value = tasks;
 
-         editTitles = titles;
-         editTasks = tasks;
-
-        editModal.style.display = "block"
-
-        function editButton () {
-            // if (editTitle !== null || editTask !== null) {
-            //     titles.textContent = editTitle;
-            //     tasks.textContent = editTask;
-            // }
-
-            // console.log(editTitle)
-            // console.log(editTask)
-            
-            // h2.textContent = `${editTitles}`;
-            // blockquote.textContent = `${editTasks}`;
-
-            editModal.style.display = "none"
-            // let newText = prompt("Edit Task",titles);
-            // if (newText !== null) {
-            //     this.textContent = newText;
-            // }
-        }
-        editBtn.addEventListener("click",editButton)
+         btn.style.display = "none";
+         editBtn.style.display = "block";
        }
 
-      
-
-
        
+       const editTask = () => {
+        if (titles !== null || tasks !== null) {
+            let  newTitles = title.value.trim()
+            let  newTasks = task.value.trim()
+           
+            h2.innerHTML = `${newTitles}`; 
+            blockquote.innerHTML = `${newTasks}`;
+
+            title.value = ""
+            task.value = ""
+
+             btn.style.display = "block";
+             editBtn.style.display = "none";
+        }
+    }
+
+
 
        const taskDelete = () => {
             article.remove()
@@ -147,6 +276,8 @@ const TaskList = () => {
       iFirst.addEventListener("click",taskComplete);
       iSecond.addEventListener("click",taskEdit);
       iThird.addEventListener("click",taskDelete);
+      editBtn.addEventListener("click",editTask);
+      
     }
 }
 
